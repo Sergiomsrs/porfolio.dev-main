f9857be38cc0f5377f80e3ca78afc0f7a19861a4
Sergio <sergiomsrs@gmail.com>
2025-06-15 00:19:46 +0200

    chore: update dependencies and add GitHub Actions workflow for deployment
    - Updated astro version from 4.1.3 to ^4.16.18 in package.json
    - Added reference to Astro types in src/env.d.ts
    - Created a new GitHub Actions workflow for deploying the Astro site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
+const isProd = process.env.NODE_ENV === 'production';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: isProd ? 'https://sergiomsrs.github.io/porfolio.dev-main/' : undefined,
+  base: isProd ? '/porfolio.dev-main/' : '/',
+  integrations: [tailwind()],
 });
